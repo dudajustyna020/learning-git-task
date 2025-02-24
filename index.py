294c587a657ffa2cd06c3f8687b820fd49febea8
Justyna Duda-Kowalczyk <dudajustyna020@gmail.com>
2025-02-24 13:12:30 +0100

    Pozdrowienia

--- index.py
+++ index.py
@@ -1,12 +1,4 @@
-shopping_list = {
-    "piekarnia":["chelb","pączek","bułki"],
-    "warzywniak":["marchew","seler","rukola"]
-}
-total_items =0
-for shop, products in shopping_list.items():
-    shop = shop.capitalize()
-    products = [product.capitalize() for product in products]
-    print(f"Idę do {shop} i kupuję tam: {','.join(products)}")
-    total_items += len(products)
-    print(f"W sumie kupuję {total_items} produktów. ")
+pozdrowienia = "Miłego tygodnia oraz samacznych pączków w czwartek"
+print(pozdrowienia)
+
 
